2cd5a0f39936a7223ada596d88e14cac9794de00
Julio Quiezi <julioquiezi@gmail.com>
2022-08-31 18:26:09 +0900

    feat: add welcome message with username

--- bot.py
+++ bot.py
@@ -2,15 +2,13 @@ import slack
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
-SLACK_TOKEN="xoxb-4006136814037-3994581778295-4QrVPQFTakFE924Em7cU73Xd"
+SLACK_TOKEN="xoxb-4006136814037-3994581778295-E5rvdQjanEUmU86S8s3jMCse"
 SIGNING_SECRET="c5be448a167d1ded05cafa2bfd98cb6b"
 
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
-
+ 
 client = slack.WebClient(token=SLACK_TOKEN)
-
-
 @ slack_event_adapter.on('message')
 def message(payload):
     print(payload)
@@ -20,11 +18,12 @@ def message(payload):
     text = event.get('text')
 
     response = client.users_profile_get(user=user_id)
-    users = response["profile.real_name"]
-    print("🟢🟢🟢🟢🟢 RESPONSE", users)
+    assert response["ok"]
+    user = response['profile']["real_name"]
+    print("🟢🟢🟢🟢🟢 USER " , user)
  
     if text == "hi":
-        client.chat_postMessage(channel=channel_id,text="Hello")
-
+        client.chat_postMessage(channel=channel_id,text="Hello, " + user + "! \nWelcome to this awesome channel! 😎")
+ 
 if __name__ == "__main__":
     app.run(debug=True)
